f80ac42f941a9ba454b19d5328f84e3845258417
AngeloCaeiro <angelo.caeiro@gmail.com>
2020-11-02 13:10:08 +0100

    epg.bat

--- epg.bat
+++ epg.bat
@@ -2,9 +2,9 @@
 
 
 @echo start github command
-git pull "https://github.com/AngeloCaeiro/epg.git"
+git commit -m "epg.bat"
 git clone "https://github.com/AngeloCaeiro/epg.git"
 git add "c:\epg\epg.bat"
-git commit -m "epg.bat"
+
 git push
 pause
