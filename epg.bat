253c57d1c2c2c83ea0c602b6ce4107f744276b60
AngeloCaeiro <angelo.caeiro@gmail.com>
2021-09-02 21:26:44 +0200

    EPG guide

---
+++ epg.bat
@@ -0,0 +1,15 @@
+c:
+cd\epg
+
+git init
+
+git add .
+
+git config --global user.email "angelo.caeiro@gmail.com"
+git config --global user.name "AngeloCaeiro"
+
+
+git pull https://github.com/AngeloCaeiro/epg.git
+git push origin main
+
+pause
